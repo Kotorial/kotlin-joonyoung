41039c6f7b260a694f04089dbaf916d6a8eede85
zayson <jymaeng95@gmail.com>
2023-02-08 13:07:16 +0900

    rename: Main to MainApplication

--- src/main/kotlin/org/kotorial/MainApplication.kt
+++ src/main/kotlin/org/kotorial/MainApplication.kt
@@ -1,7 +1,17 @@
+package org.kotorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MainApplication
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    runApplication<MainApplication>(*args)
 }
